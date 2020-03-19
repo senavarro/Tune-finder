1de5b31131475cfffd18197e5bf037947078f8bd
Sergi Navarro <serginavarro.ro@gmail.co>
2020-03-19 15:10:37 +0000

    First commit
    Project uploaded

---
+++ js/app.js
@@ -0,0 +1,40 @@
+import * as UI from './interfaz.js';
+import { API } from './api.js';
+
+UI.formularioBuscar.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    //Obtener datos del formulario
+    const artista = document.querySelector('#artista').value,
+          cancion = document.querySelector('#cancion').value
+
+    if(artista === '' || cancion === '') {
+        //Usuario deja campos vacíos, mostrar error 
+        UI.divMensajes.innerHTML = "Error! The fields can't be empty. Please fill them up";
+        UI.divMensajes.classList.add('error');
+        setTimeout(() => {
+            UI.divMensajes.innerHTML = "";
+            UI.divMensajes.classList.remove('error'); 
+        }, 3000);
+    }else{
+        const api = new API(artista, cancion);
+        api.consultarAPI()
+            .then(data => {
+                if(data.respuesta.lyrics) {
+                    //La cancion existe
+                    const letra = data.respuesta.lyrics;
+                    UI.divResultado.textContent = letra;
+                }else{
+                    //La cancion no existe
+                    UI.divMensajes.innerHTML = "La canción no existe. Prueba con otra búsqueda";
+                    UI.divMensajes.classList.add('error');
+                    setTimeout(() => {
+                        UI.divMensajes.innerHTML = "";
+                        UI.divMensajes.classList.remove('error'); 
+                        UI.formularioBuscar.reset();
+                    }, 3000);
+                }
+            })
+    }
+
+} )
